52b81a15c91e92521758e2571f6b4c3a477acf99
ItsChrisHickman <102430192+ItsChrisHickman@users.noreply.github.com>
2023-07-11 16:31:17 -0500

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -33,45 +32,45 @@ export default function App() {
 
   return (
     <div className="App">
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <header>
-          <h1>Showcase Settings</h1>
-          <a href="https://goshow.me">GoShow.me</a>
-        </header>
+      <CssBaseline />
+      <header>
+        <h1>Showcase Settings</h1>
+        <a href="https://goshow.me">GoShow.me</a>
+      </header>
+      <Container maxWidth="xl">
         <Showcase onConnect={handleConnect} />
-        <Container maxWidth="md">
-          <Box>
-            <p>
-              The Settings Namespace allows developers access to internal
-              settings within Showcase. These internal settings are unsupported
-              and may change. This playground will allow developers to
-              experiment with all known settings.
-            </p>
-          </Box>
-          <Box>
-            <SettingsList sdk={sdk} />
-          </Box>
-          <Box>
-            <h3>Changelog</h3>
-            <ul>
-              <li>2023/07/11 - Migrated to Refactored with React.js w/MUI</li>
-              <li>2023/07/09 - Refactored with React.js</li>
-              <li>
-                2022/12/20 - Added deserialization for fill and stroke -- Cannot
-                figure out how to get these values to update however...
-              </li>
-              <li>2022/10/27 - Text Inputs and Select Boxes are now active.</li>
-            </ul>
-            <p>
-              <b>To-Do</b>
-            </p>
-            <ul>
-              <li>Tooltips on settings to explain what the setting does.</li>
-            </ul>
-          </Box>
-        </Container>
-      </ThemeProvider>
+      </Container>
+      <Container maxWidth="md">
+        <Box>
+          <p>
+            The Settings Namespace allows developers access to internal settings
+            within Showcase. These internal settings are unsupported and may
+            change. This playground will allow developers to experiment with all
+            known settings.
+          </p>
+        </Box>
+        <Box>
+          <SettingsList sdk={sdk} />
+        </Box>
+        <Box>
+          <h3>Changelog</h3>
+          <ul>
+            <li>2023/07/11 - Migrated to Refactored with React.js w/MUI</li>
+            <li>2023/07/09 - Refactored with React.js</li>
+            <li>
+              2022/12/20 - Added deserialization for fill and stroke -- Cannot
+              figure out how to get these values to update however...
+            </li>
+            <li>2022/10/27 - Text Inputs and Select Boxes are now active.</li>
+          </ul>
+        </Box>
+        <Box>
+          <h3>To-Do</h3>
+          <ul>
+            <li>Tooltips on settings to explain what the setting does.</li>
+          </ul>
+        </Box>
+      </Container>
     </div>
   );
 }
